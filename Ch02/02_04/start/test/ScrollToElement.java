6cae90683e40250da0d7930dc626c425fedcb250
Karina Leon <alcarazkarina11@gmail.com>
2021-09-10 17:20:49 -0500

    Initial commt

---
+++ Ch02/02_04/start/test/ScrollToElement.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ScrollToElement {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/scroll");
+        WebElement name = driver.findElement(By.id("name"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(name);
+        name.sendKeys("Karina Leon");
+
+        WebElement date= driver.findElement(By.id("date"));
+        date.sendKeys("09/09/2021");
+
+        driver.quit();
+    }
+}
